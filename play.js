1a6b43aafbd385e6649df52531bfec7b2f876491
Dustin Grof <dustingrof@gmail.com>
2022-03-16 16:35:41 -0700

    seperate input into its own file, export, then import it into play.js

--- play.js
+++ play.js
@@ -1,22 +1,9 @@
+
 const net = require("net");
 const connect = require('./client');
+const setupInput = require("./input");
 
 console.log("Connecting ...");
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-const handleUserInput = function(data) {
-  // your code here
-  if (data === '\u0003') {
-    process.exit();
-  }
-};
 
 connect();
 setupInput();
